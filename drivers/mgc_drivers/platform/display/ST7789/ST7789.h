39438f5a139718172a8a5b77cfa9d925848f22b6
nyannkov <54160654+nyannkov@users.noreply.github.com>
2025-06-17 04:02:35 +0900

    refactor port responsibilities and minimize API surface

--- drivers/mgc_drivers/platform/display/ST7789/ST7789.h
+++ drivers/mgc_drivers/platform/display/ST7789/ST7789.h
@@ -29,7 +29,7 @@ static inline uint16_t ST7789_get_height(void) {
     return MGC_DRIVERS_ST7789_HEIGHT;
 }
 
-void ST7789_init(uint32_t clock_rate);
+void ST7789_init(uint32_t spi_clock_rate);
 void ST7789_reset(void);
 void ST7789_transfer_region(uint8_t *buffer, size_t len, uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1);
 
---
+++ drivers/mgc_drivers/platform/display/ST7789/ports/ST7789_port.h
@@ -0,0 +1,39 @@
+/*
+ * MIT License
+ * (https://opensource.org/license/mit/)
+ *
+ * Copyright (c) 2025 nyannkov
+ */
+#ifndef MGC_DRIVERS_ST7789_PORT_H
+#define MGC_DRIVERS_ST7789_PORT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "mgc/common/common.h"
+
+#define UBYTE   uint8_t
+#define UWORD   uint16_t
+#define UDOUBLE uint32_t
+
+extern const unsigned int LCD_RST_PIN;
+extern const unsigned int LCD_DC_PIN;
+extern const unsigned int LCD_BL_PIN;
+extern const unsigned int LCD_CS_PIN;
+extern const unsigned int LCD_CLK_PIN;
+extern const unsigned int LCD_MOSI_PIN;
+
+void ST7789_PORT__Init(uint32_t spi_clock_rate);
+void ST7789_PORT__Delay_ms(UDOUBLE xms);
+void ST7789_PORT__Digital_Write(UWORD Pin, UBYTE Value);
+void ST7789_PORT__SET_PWM(uint8_t Value);
+void ST7789_PORT__GPIO_Mode(UWORD Pin, UWORD Mode);
+void ST7789_PORT__SPI_WriteByte(uint8_t Value);
+void ST7789_PORT__SPI_Write_nByte(uint8_t pData[], uint32_t Len);
+
+#ifdef __cplusplus
+}/* extern "C" */
+#endif
+
+#endif/*MGC_DRIVERS_ST7789_PORT_H*/
