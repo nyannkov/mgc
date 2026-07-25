72d1332d2065508e17c7f9653a9cb8f0f49a30c6
nyannkov <54160654+nyannkov@users.noreply.github.com>
2026-07-25 14:16:47 +0900

    Update devtest/test_game

--- devtest/test_game/scene/scene_core.cpp
+++ devtest/test_game/scene/scene_core.cpp
@@ -41,17 +41,20 @@ bool event_update(IEventObjects* events) {
     return is_control_locked;
 }
 
-void update_movement(SceneContext& scx) {
-    bool just_off_board = false;
+bool is_just_off_board(SceneContext& scx) {
     if ( scx.objs ) {
         for ( auto* carrier : scx.objs->carriers() ) {
             if ( carrier->off_board_state() == carrier::Carrier::OffBoardState::JustOffBoard ) {
-                just_off_board = true;
-                break;
+                return true;
             }
         }
     }
-    scx.player.update_movement(just_off_board);
+    return false;
+}
+
+void update_movement(SceneContext& scx) {
+
+    scx.player.update_movement(is_just_off_board(scx));
 
     if ( scx.objs ) {
         for ( auto* block : scx.objs->blocks() ) {
@@ -263,16 +266,7 @@ void update(
         }
     }
 
-    bool just_off_board = false;
-    if ( scx.objs ) {
-        for ( auto* carrier : scx.objs->carriers() ) {
-            if ( carrier->off_board_state() == carrier::Carrier::OffBoardState::JustOffBoard ) {
-                just_off_board = true;
-                break;
-            }
-        }
-    }
-    scx.player.update_animation(talkflow.in_progress(), just_off_board);
+    scx.player.update_animation(talkflow.in_progress(), is_just_off_board(scx));
 
     if ( scx.objs ) {
         for ( auto* enemy : scx.objs->enemies() ) {
